49133c0b97184eb4a5d80716a4d892365c8831f1
Brayan <bscrsecundaria@gmail.com>
2025-10-02 16:40:19 -0500

    Definicion inicial

--- habitaciones/habitaciones.py
+++ habitaciones/habitaciones.py
@@ -1,1 +1,12 @@
-print("Prueba de funcionamiento")
+# Módulo de Habitaciones
+# Día 1: Definición inicial
+
+class Habitacion:
+    def __init__(self, id_habitacion, tipo, tarifa, estado="disponible"):
+        self.id_habitacion = id_habitacion
+        self.tipo = tipo
+        self.tarifa = tarifa
+        self.estado = estado  # disponible, ocupada, mantenimiento
+
+    def __str__(self):
+        return f"Habitación[{self.id_habitacion}] - {self.tipo} (${self.tarifa}) [{self.estado}]"
