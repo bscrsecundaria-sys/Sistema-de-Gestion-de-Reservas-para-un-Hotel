937eb0e83b09dcbf8f3e59c143d721dc943ae31e
JCMR05 <juancamilomorarangel@gmail.com>
2025-10-02 16:41:51 -0500

    definicion inicial

--- clientes/clientes.py
+++ clientes/clientes.py
@@ -1,1 +1,14 @@
 print("Prueba clientes")
+# Módulo de Clientes
+# Día 1: Definición inicial
+
+class Cliente:
+    def _init_(self, id_cliente, nombre, correo):
+        self.id_cliente = id_cliente
+        self.nombre = nombre
+        self.correo = correo
+        self.historial_reservas = []
+
+    def _str_(self):
+        return f"Cliente[{self.id_cliente}] - {self.nombre} ({self.correo})"
+    
